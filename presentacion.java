c65fd00b49f6d859be14532cfda60409c798390a
pedrofalso galera <pagalerag01@educarex.es>
2024-06-06 11:06:08 +0000

    he puesto el mio

--- presentacion.java
+++ presentacion.java
@@ -5,5 +5,10 @@
  {
      public static void main(String[] args) {
          System.out.println("Hola mi nombre es David!");
-     }
+    
+         
+            System.out.println("Hola mi nombre es Pedro Galera");
+    
+    
+        }
  }
